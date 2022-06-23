2fbd90ed250f3adae83b09d30ea3eff6d3b8bd7d
JavaDen44 <dem.denis44@gmail.com>
2022-06-24 00:28:48 +0300

    Moved methods from class Location to classMap Island, changed the method of filming Locations(), implemented the move() method.

--- src/java/com/javarush/island/model/animals/carnivore/Boa.java
+++ src/java/com/javarush/island/model/animals/carnivore/Boa.java
@@ -5,7 +5,7 @@ import com.javarush.island.model.animals.abstracts.CarnivoreAnimal;
 public class Boa extends CarnivoreAnimal {
 
     public Boa() {
-        super(15,30,1,3);
+        super(15, 30, 1, 3);
     }
 
     @Override
